8db53c34131c6db3c37d711bacfe9ac0a4b1d337
Dominik Serafin <DominikSerafin@users.noreply.github.com>
2015-03-25 10:09:10 +0100

    Update beaqnin.js

--- js/beaqnin.js
+++ js/beaqnin.js
@@ -20,16 +20,14 @@ $(document).ready(function(){
 	// HOTJAR BETA
 	//----------------------------------------------------------------------------------//
 
-	<script>
-	    (function(f,b){
-	        var c;
-	        f.hj=f.hj||function(){(f.hj.q=f.hj.q||[]).push(arguments)};
-	        f._hjSettings={hjid:23430, hjsv:3};
-	        c=b.createElement("script");c.async=1;
-	        c.src="//static.hotjar.com/c/hotjar-23430.js?sv=3";
-	        b.getElementsByTagName("head")[0].appendChild(c); 
-	    })(window,document);
-	</script>
+	(function(f,b){
+		var c;
+		f.hj=f.hj||function(){(f.hj.q=f.hj.q||[]).push(arguments)};
+		f._hjSettings={hjid:23430, hjsv:3};
+		c=b.createElement("script");c.async=1;
+		c.src="//static.hotjar.com/c/hotjar-23430.js?sv=3";
+		b.getElementsByTagName("head")[0].appendChild(c); 
+	})(window,document);
 
 	//----------------------------------------------------------------------------------//
 	// TWITTER API
