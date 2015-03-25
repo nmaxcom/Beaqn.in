5dc32e17e936bfc8babd1474e34d9040c0d22d24
Dominik Serafin <DominikSerafin@users.noreply.github.com>
2015-03-25 10:07:53 +0100

    Update beaqnin.js
    Testing Hotjar.com beta analytics

--- js/beaqnin.js
+++ js/beaqnin.js
@@ -16,6 +16,20 @@ $(document).ready(function(){
 	ga('create', 'UA-46688343-7', 'auto');
 	ga('send', 'pageview');
 
+	//----------------------------------------------------------------------------------//
+	// HOTJAR BETA
+	//----------------------------------------------------------------------------------//
+
+	<script>
+	    (function(f,b){
+	        var c;
+	        f.hj=f.hj||function(){(f.hj.q=f.hj.q||[]).push(arguments)};
+	        f._hjSettings={hjid:23430, hjsv:3};
+	        c=b.createElement("script");c.async=1;
+	        c.src="//static.hotjar.com/c/hotjar-23430.js?sv=3";
+	        b.getElementsByTagName("head")[0].appendChild(c); 
+	    })(window,document);
+	</script>
 
 	//----------------------------------------------------------------------------------//
 	// TWITTER API
